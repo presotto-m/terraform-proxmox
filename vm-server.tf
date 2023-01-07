6760c3dce6ef5c86bea21e780fee2f7a3ab1d7f9
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-01-06 22:16:01 -0300

    Update vm-server.tf

--- vm-server.tf
+++ vm-server.tf
@@ -22,8 +22,8 @@ resource "proxmox_vm_qemu" "teste-terraform" {
     }
 
     os_type = "cloud-init"
-    ipconfig0 = "ip=192.168.170.30/24,gw=192.168.170.1"
+    ipconfig0 = ""
     ciuser = "root"
-    sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAn9XfNr1csiUhVbn/ThHasPRQsyaH35+Y0YZZkdx7Z8cuU9FIiIK5RD6XpEyNvPAcA+SCAB21aJn3yri6LuWy3i9shIstFWqmLjHfdtOfKJu3Z3UxbHdQBWG16RUhAh8r/BFHrzqGUA0PHfhZsSVWxY6tnPQcjp8ICedHOtxNUW/Synd/Ws8MgutuiXibmGkW02EPduVAgFYxP0koqfFMN6qdoxS9nBb2liCH+04f+H1Xh1vVXNgCk33pB4iWfvsAXQ0gVCnYYgOWQyO/aZkdmZuwRcH+ANdbvxYahLVxMC4Zet+E3dtphnL7BMB9oQFYQNQGhtTdQ1GcSowvrcQBlw== key-mp"
+    sshkeys = ""
     
 } 
